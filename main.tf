17902c8dc5edde2b02b88f0abdedeca6c1838219
Constantine19 <czelmanovich@gmail.com>
2023-11-05 17:40:50 +0200

    make a resource

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-  provider "aws" {
-    region = "us-east-1"
-  }
-  locals {
-    project_name = "Kosta"
-  }
-}
 
 resource "aws_s3_bucket" "kosta_bucket" {
   bucket = "kosta-bucket-from-tf"
