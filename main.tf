8303f862811ad16100b93f3557e7911672082a8b
Constantine19 <czelmanovich@gmail.com>
2023-11-05 17:21:36 +0200

    add locals

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  provider "aws" {
+    region = "us-east-1"
+  }
+  locals {
+    project_name = "Kosta"
+  }
+}
+
 resource "aws_s3_bucket" "kosta_bucket" {
   bucket = "kosta-bucket-from-tf"
 }
