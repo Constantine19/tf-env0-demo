6f9f627e40909bf92757c9c6eb6d0cd2839b7df8
Konstantin Zelmanovich <Constantine19>
2024-01-03 14:14:17 +0200

    add another bucket

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@
 resource "aws_s3_bucket" "kosta_bucket" {
   bucket = "kosta-bucket-from-tf"
 }
+
+resource "aws_s3_bucket" "kosta_bucket_testing_for_state_migration" {
+  bucket = "kosta_bucket_testing_for_state_migration"
+}
