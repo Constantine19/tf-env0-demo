d0c39dbf406e98a266e0e45033e4c5bcd1dd213f
Konstantin Zelmanovich <Constantine19>
2024-01-03 14:28:18 +0200

    add the first bucket back

--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
-resource "aws_s3_bucket" "demo_bucket1" {
-  bucket = "kosta_1"
-}
 
-#Resource to create s3 bucket
-resource "aws_s3_bucket" "demo_bucket2" {
-  bucket = "kosta_2"
-}
-
-#Resource to create s3 bucket
-resource "aws_s3_bucket" "demo_bucket3" {
-  bucket = "kosta_3"
+resource "aws_s3_bucket" "kosta_bucket" {
+  bucket = "kosta-bucket-from-tf"
 }
