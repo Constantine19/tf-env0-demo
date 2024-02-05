e7863da8889363c008afcee3b0c2a92cef42b981
Konstantin Zelmanovich <Constantine19>
2024-02-05 14:38:12 +0200

    add root folders

---
+++ root_2/main.tf
@@ -0,0 +1,4 @@
+
+resource "aws_s3_bucket" "kosta_bucket" {
+  bucket = "kosta-bucket-from-terraform"
+}
---
+++ root_2/provider.tf
@@ -0,0 +1,6 @@
+## This is a comment to test the PR-Plan
+
+
+provider "aws" {
+    region = "us-east-1"
+}
